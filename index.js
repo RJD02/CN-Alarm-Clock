d8aa98dfb82a0889ae132707675b219cfd581ac1
RJD02 <dulangeraviraj@gmail.com>
2023-07-21 22:05:19 +0530

    Update the modal actions

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const addNewBtn = document.querySelectorAll(".add-new-alarm");
 
-addNewBtn.forEach((val) =>
-  val.addEventListener("click", () => {
-    console.log("creating new alarm");
-    openModal();
-  })
-);
 // Modal opening and closing logic
 const modalDialog = document.querySelector("#dialog");
 const modalCloseBtn = document.querySelector("#close");
 const overlay = document.querySelector("#overlay");
+
+// the id for alarms
+let id = 1;
+const countdownContainer = document.querySelector(".count-down");
+
+addNewBtn.forEach((val) =>
+  val.addEventListener("click", () => {
+    console.log("creating new alarm");
+
+    const alarm = new AlarmElement();
+    alarm.handle();
+  })
+);
 
 // show the overlay and the dialog
 const openModal = () => {
@@ -33,71 +40,54 @@ const closeModal = () => {
   overlay.classList.add("hidden");
 };
 
-// const setAlarmBtn = document.querySelector("#setAlarmBtn");
-// const alarmTimeInput = document.querySelector("#alarmTime");
-// const countdownContainer = document.querySelector("#countdown");
-// const countdownTimeSpan = document.querySelector("#countdownTime");
+class AlarmElement {
+  constructor() {
+    openModal();
+    this.id = id++;
+    this.isOn = true;
+    this.soundName = "first";
+    this.alarmInterval;
+    this.timeRemaining;
+  }
+  okhandler() {
+    const hours = document.querySelector("#hours");
+    const minutes = document.querySelector("#minutes");
+    const soundName = document.querySelector("#sound-select");
 
-// let alarmInterval; // value input by user
-// let audio = new Audio();
+    const am = document.querySelector("#AM");
+    const pm = document.querySelector("#PM");
 
-// const playAudio = () => {
-//   audio.src = "./public/assets/audio/alarm-clock-project.mp3";
-//   audio.autoplay = true;
-//   //   setTimeout(() => {
-//   //     audio.pause();
-//   //   }, 1000);
-// };
+    console.log("AM", am.checked);
+    console.log("PM", pm.checked);
+    if (am.checked === pm.checked) {
+      const message = document.querySelector(".error");
+      message.classList.remove("hidden");
+      message.textContent = `Please select ${
+        am.checked === false ? "at least am or pm" : "at most one of am or pm"
+      }`;
 
-// setAlarmBtn.addEventListener("click", () => {
-//   const alarmTime = alarmTimeInput.value;
-
-//   if (!alarmTime) {
-//     alert("Please set the alarm time!");
-//     return;
-//   }
-
-//   const [hours, minutes] = alarmTime.split(":").map(Number);
-//   const now = new Date();
-//   const alarmDateTime = new Date(
-//     now.getFullYear(),
-//     now.getMonth(),
-//     now.getDate(),
-//     hours,
-//     minutes,
-//     0
-//   );
-//   const timeRemaining = alarmDateTime - now;
-//   if (timeRemaining < 0) {
-//     alert("please set a time in the future");
-//     return;
-//   }
-
-//   alarmInterval = setInterval(updateCountDown, 1000, alarmDateTime);
-
-//   countdownContainer.style.display = "block";
-//   alarmTimeInput.value = "";
-// });
-
-// const updateCountDown = (alarmDateTime) => {
-//   const now = new Date();
-//   const timeRemaining = alarmDateTime - now;
-//   console.log(timeRemaining);
-
-//   if (timeRemaining <= 0) {
-//     playAudio();
-//     clearInterval(alarmInterval);
-//     alert("Times up! Alarm triggered");
-//     countdownContainer.style.display = "none";
-//     return;
-//   }
-//   const hours = Math.floor(timeRemaining / (1000 * 60 * 60));
-//   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-//   const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-//   countdownTimeSpan.textContent = `${hours
-//     .toString()
-//     .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
-//     .toString()
-//     .padStart(2, "0")}`;
-// };
+      return;
+    }
+    // createNewAlarmHTMLElement();
+    hours.value = 0;
+    minutes.value = 0;
+    am.checked = false;
+    pm.checked = false;
+    closeModal();
+  }
+  cancelHandler() {
+    closeModal();
+  }
+  handle() {
+    const okBtn = document.querySelector("#ok");
+    const cancelBtn = document.querySelector("#close");
+    okBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      this.okhandler();
+    });
+    cancelBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      this.cancelHandler();
+    });
+  }
+}
